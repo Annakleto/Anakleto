152a930e317509a3453e8403438a4ff97acd08a3
anna <>
2018-11-29 00:26:15 +0100

    no message

--- main.js
+++ main.js
@@ -13,21 +13,9 @@ $( document ).ready(function() {
    		}
     //if enter button is pressed, the room link is called
 		else if (e.keyCode == 13) { // 13 = enter
-<<<<<<< HEAD
 			alertCall();
    		}
 	});
-=======
-			on();
-			$( "#link" ).html("keycode 13");
-   		}
-	});
-    //if the input lose focus, the room link is called
-	$( "#roomNameInput" ).change(function() {
-  		on();
-		
-	});
->>>>>>> master
     
     //the input value is salved into a variable that becomes the calling room name
     function goToRoomURL(){
@@ -36,7 +24,6 @@ $( document ).ready(function() {
 		//window.location = "https://appr.tc/r/" + number;
         document.getElementById("videobox").innerHTML = "<iframe src=\"https://appr.tc/r/" + number + "\" id=\"appr\"></iframe>";
     }
-<<<<<<< HEAD
 	
 	function alertCall(){
 		var number = $.trim($("#roomNameInput").val());
@@ -48,18 +35,6 @@ $( document ).ready(function() {
 			$( "#number" ).html("Error");
 			document.getElementById("overlay").style.display = "block";
 		}
-=======
-    
-    function on() {
-    	document.getElementById("overlay").style.display = "block";
-	}
-
-	function off() {
-    	document.getElementById("overlay").style.display = "none";
-	}
-	
-<<<<<<< HEAD
->>>>>>> master
 		
     }
 	
